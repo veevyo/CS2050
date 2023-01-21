c5a00f74af2cca998e3809b32d5b473547a763be
vivi <41368076+veevyo@users.noreply.github.com>
2023-01-21 16:35:41 -0600

    finished prelab1

--- prelab1/main.c
+++ prelab1/main.c
@@ -1,15 +1,19 @@
 #include "prelab1.h"
 
+//used for testing
 void printArray(int * array, int size);
 
 int main(void) {
+	//test values, feel free to change
 	int distinctCount, largest, smallest = 0;
-	int array[] = {17, 33, 2, 2, 14, 9, 4};
-	getArrayInfo(array, 8, &distinctCount, &largest, &smallest);
-	printArray(array, 8);
+	int array[] = {3, 2, 1, 0, 0, 0, 22, 2};
+	int size = sizeof(array) / sizeof(array[0]);
+	getArrayInfo(array, size, &distinctCount, &largest, &smallest);
+	printArray(array, size);
 	printf("Distinct counter is: %d\nLargest num is %d\nSmallest num is %d\n", distinctCount, largest, smallest);
 }
 
+//print arr for testing purposes in the console
 void printArray(int * array, int size) {
 	int i;
 	printf("{");
--- prelab1/prelab1.c
+++ prelab1/prelab1.c
@@ -6,18 +6,27 @@ void getArrayInfo(int *array, int size, int * distinctCount, int * largest, int 
 	*distinctCount = 0;
 	*smallest = array[0];
 	*largest = array[0];
+
+	//most of the action happens in this for loop
 	for (i = 0; i < size; i++) {
-		for (j = i + 1; j < size; j++) {
-			if (array[i] != array[j]) {
-				*distinctCount += 1;
+		//check against other values and see if there are duplicates in the arr
+		for (j = 0; j < i; j++) {
+			if (array[i] == array[j]) {
+				break;
 			}
 		}
+		if (i == j) {
+			*distinctCount += 1;
+		}
+		//kinda self explanatory code
 		if (array[i] < *smallest) {
 			*smallest = array[i];
 		}
 		if (array[i] > *largest) {
 			*largest = array[i];
 		}
+
+	//the counter should be 1 if there's only one value in the whole thing just repeated
 	}
 	if (*distinctCount == 0) {
 		*distinctCount = 1;
