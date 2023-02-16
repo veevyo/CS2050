7010dd67744f16361398128881cea97c0cf8f475
vivi <41368076+veevyo@users.noreply.github.com>
2023-02-15 22:12:12 -0600

    almost done with prelab4

--- prelab4/main.c
+++ prelab4/main.c
@@ -8,4 +8,7 @@ int main(void) {
 	Employee * array = readEmployeeArray(fp);
 	printf("employee [1] id = %d\n", array[1].empID);
 	printEmpArray(array, 3);
+	printf("we are going to change employee 2231's salary to $30,000.33\n");
+	setEmpSalary(array, 2231, 30000.33);
+	printEmpArray(array, 3);
 }	
--- prelab4/prelab4.c
+++ prelab4/prelab4.c
@@ -15,11 +15,13 @@ Employee * readEmployeeArray(FILE *fp) {
 	int i;
 	int length = getEmpArraySize(fp);
 	printf("length is %d\n", length);
-	Employee * array = malloc(sizeof(Employee) * length);
+	Employee * array = malloc(sizeof(int) + (sizeof(Employee) * length));
 	printf("if malloc fails, i won't print!\n");
+	array[0].empID = length;
+	array++;
+	printf("size is %d\n", array[-1].empID);
 	for (i = 0; i < length; i++) {
 		array[i] = readEmployee(fp);
-		//printf("%d, %d, %.2f\n", array[i].empID, array[i].jobType, array[i].salary);
 	}
 	return array;
 
@@ -34,19 +36,44 @@ Employee readEmployee(FILE *fp) {
 	return employee;
 }
 
-/*void printEmployee(Employee employee) {
-	printf("[");
-	printf("%d, ", employee.empID);
-	printf("%d, ", employee.jobType);
-	printf("%f, ", employee.salary);
-	printf("]");
-}*/
-
 void printEmpArray(Employee * array, int length) {
 	int i;
-	printf("{");
+	printf("{ ");
 	for (i = 0; i < length; i++) {
 		printf("[%d, %d, %.2f] ", array[i].empID, array[i].jobType, array[i].salary);
 	}
 	printf("}\n");
+}
+
+int scanForEmpID(Employee * employee, int empID, int * pos) {
+	int length = employee[-1].empID;
+	int i;
+	for (i = 0; i < length; i++) {
+		if (employee[i].empID == empID) {
+			*pos = i;
+			return 0;
+		}
+	}
+	return 1;
+}
+
+int setEmpSalary(Employee * employee, int empID, float salary) {
+	int pos = 0;
+	int check = scanForEmpID(employee, empID, &pos);
+	printf("printing pos so it works %d\n", pos);
+	if (check == 0) {
+		employee->salary = salary;
+		return 0;
+	}
+	return 1;
+}
+
+int getEmpSalary(Employee * employee, int empID, float *salary) {
+	int pos = 0;
+	int check = scanForEmpID(employee, empID, &pos);
+	if (check == 0) {
+		*salary = employee[pos].salary;
+		return 0;
+	}
+	return 1;
 }
